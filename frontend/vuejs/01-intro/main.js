260431a91515d97998ac11a9da02396182a40892
MDevoldere <mdevoldere@users.noreply.github.com>
2022-04-08 10:22:14 +0200

    intro vue

--- frontend/vuejs/01-intro/main.js
+++ frontend/vuejs/01-intro/main.js
@@ -3,16 +3,35 @@ const app = {
     data() {
         return {
             message: 'Hello world !',
-            nombre: 0
+            nombre: 0,
+            prenom: '',
+            movies: []
         }
     },
 
-    mounted() {
+    async mounted() {
+
         setInterval(() => {
             this.nombre++;
         }, 1000);
+
+        let response = await fetch('https://api.devoldere.net/dataset/movies.json');
+        this.movies = await response.json();      
+        
+        setTimeout(() => {
+            this.movies.shift();
+            this.movies[0].movie_title = "Toto chez les DWWM, le retour !";
+        }, 5000);
+    },
+
+    methods: {
+        reinitialiser() {
+            this.nombre = 0;
+            alert(this.prenom);
+        }
     }
 
-}
+} // fin de app
+
 
 Vue.createApp(app).mount('#toto');
